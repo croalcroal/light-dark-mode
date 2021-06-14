ba9e82f330d7278931df852a7f43b0b3975ed58f
croalcroal <47533795+croalcroal@users.noreply.github.com>
2021-06-14 14:05:30 -0400

    update for dry code
    repetative doc.getel by id. changed to function

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+function el_by_id(s) {
+	return document.getElementById(s);
+}
+
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
-const nav = document.getElementById('nav');
-const toggleIcon = document.getElementById('toggle-icon');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
-const textBox = document.getElementById('text-box');
+const nav = el_by_id('nav');
+const toggleIcon = el_by_id('toggle-icon');
+const image1 = el_by_id('image1');
+const image2 = el_by_id('image2');
+const image3 = el_by_id('image3');
+const textBox = el_by_id('text-box');
 
 // Dark or Light images
 function imageMode(color) {
